7aa445fc7842a26185e48e3a022e717b95fe1011
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-09 12:11:07 -0700

    Add files via upload

--- Counting Bits.py
+++ Counting Bits.py
@@ -12,3 +12,9 @@ class Solution:
                 offset = i
             dp[i] = 1 + dp[i - offset]
         return dp
+
+        # Solution 2
+        # counter = [0]
+        # for i in range(1, n+1):
+        #     counter.append(counter[i >> 1] + i % 2)
+        # return counter
